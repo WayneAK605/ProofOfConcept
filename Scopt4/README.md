0910bbb5343fecc033ff4fdd0d9556003125de5b
Wayne Huang <whuang@605.tv>
2021-02-07 15:11:20 -0500

    Clean up code and add README

---
+++ Scopt4/README.md
@@ -0,0 +1,18 @@
+## Scopt 4.0 POC
+
+###Test Parameters
+You can but the following into Intellij's application argument.
+
+*Note: File = place would be success - It will print "place".*
+```
+"--File"
+"visit"
+"--DeltaBasePath"
+"/src/Delta/"
+"--InputBasePath"
+"/src/Input"
+"--OutputBasePath"
+"/src/Output/"
+"--CrosswalkPath"
+"/src/Input/Crosswalk"
+```
--- Scopt4/src/main/scala/com/scopt4poc/Scopt4Poc.scala
+++ Scopt4/src/main/scala/com/scopt4poc/Scopt4Poc.scala
@@ -1,56 +1,60 @@
 package com.scopt4poc
-
-
-import com.scopt4poc.params.{CommonParams, InputParameters, PlaceParams, VisitParams}
 import scopt._
+import com.scopt4poc.params.{CommonParams, InputParameters, PlaceParams, VisitParams}
 
 object Scopt4Poc {
   def main(args: Array[String]) = {
     var aFile = ""
 
+    //Get File Type
     args.sliding(2, 2).toList.collect {
       case Array("--File", argFile: String) => aFile = argFile
     }
 
+    //Parse parameters from args
+    POCParser.parseParameters(aFile, args)
 
-    val parser = aFile match {
-        case "place" => {
-          OParser.sequence(
-            PlaceParams.parsePlaceParams[InputParameters],
-            CommonParams.parseCommonParams[InputParameters]
-          )
-        }
-        case "visit" => {
-          OParser.sequence(
-            VisitParams.parseVisitParams[InputParameters],
-            CommonParams.parseCommonParams[InputParameters]
-          )
-        }
-      }
-
-
-    val init = InputParameters(null)
-
-    lazy val params = parseParameters (args)
-
+    val params = POCParser.parseParameters (aFile, args)
     println(params.file)
+  }
+}
 
-    def parseParameters(
-               args: Array[String],
-               effectHandler: Option[OEffectSetup] = None): InputParameters = {
-      val (params, effects) = OParser.runParser(parser, args, init)
+object POCParser {
+  val init = InputParameters(null)
 
-      // scopt advises to override terminate (we shouldn't exit(1) a spark application)
-      OParser.runEffects(
-        effects,
-        effectHandler.getOrElse(new DefaultOEffectSetup {
-          override def terminate(exitState: Either[String, Unit]): Unit = () //
-        }))
+  def parseParameters(
+                       file: String,
+                       args: Array[String],
+                       effectHandler: Option[OEffectSetup] = None): InputParameters = {
 
-      params.getOrElse {
-        throw new IllegalArgumentException(
-          "Spark job submission has failed: " + args.mkString(" "))
+    val parser = file match {
+      case "place" => {
+        OParser.sequence(
+          PlaceParams.parsePlaceParams[InputParameters],
+          CommonParams.parseCommonParams[InputParameters]
+        )
       }
+      case "visit" => {
+        OParser.sequence(
+          VisitParams.parseVisitParams[InputParameters],
+          CommonParams.parseCommonParams[InputParameters]
+        )
+      }
+      case _ => throw new IllegalArgumentException(s"Unknown file is passed")
+    }
+
+    val (params, effects) = OParser.runParser(parser, args, init)
+
+    // scopt advises to override terminate (we shouldn't exit(1) a spark application)
+    OParser.runEffects(
+      effects,
+      effectHandler.getOrElse(new DefaultOEffectSetup {
+        override def terminate(exitState: Either[String, Unit]): Unit = () //
+      }))
+
+    params.getOrElse {
+      throw new IllegalArgumentException(
+        "Spark job submission has failed: " + args.mkString(" "))
     }
   }
 }
